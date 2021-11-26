35a3a87ffe2e274332472a9c84c59d2e9fb783d7
savitasawant123 <sawantsavita23@gmail.com>
2021-11-26 17:56:48 +0530

    Add project files.

---
+++ MVCCoreAppllication/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreAppllication.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "List of all books";
+        }
+        public string GetBook(int id)
+        {
+            return $"Book with id={id}";
+        }
+        public string SearchBook(string bookname,string authorname)
+        {
+            return $"Book Name is {bookname} & Author Name is {authorname}";
+        }
+    }
+}
